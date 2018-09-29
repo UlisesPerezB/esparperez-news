7fdcde90a4a37d69c71324767992661bbd27d721
Ulises P <ulisespb08@gmail.com>
2018-09-29 12:11:45 -0500

    Se añade una primera instancia del Navigation Drawer

--- app/src/main/java/com/perez/ulises/esparpereznews/main/MainActivity.java
+++ app/src/main/java/com/perez/ulises/esparpereznews/main/MainActivity.java
@@ -2,39 +2,32 @@ package com.perez.ulises.esparpereznews.main;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.perez.ulises.esparpereznews.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements MainInterface.IMainClickListener {
-
-    String[] mItems;
-
+public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.drawer_layout)
-    DrawerLayout mDrawerLayout;
+    DrawerLayout mDrawer;
+    @BindView(R.id.toolbar_main)
+    Toolbar toolbar;
 
-    @BindView(R.id.list_drawer)
-    ListView mListView;
+    private ActionBarDrawerToggle toggle;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         ButterKnife.bind(this);
-        mItems = getResources().getStringArray(R.array.navigation_drawer_items_array);
-        mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.item_drawer, mItems));
-
-    }
-
-    @Override
-    public void onClick() {
-        Toast.makeText(this, "Click", Toast.LENGTH_LONG);
+        toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,R.string.drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
